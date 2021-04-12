e6e842dc3fd0338a284b6c47958b5fc975b77ec6
Will Hanlen <will.hanlen@dualitysys.com>
2021-04-12 09:33:39 -0600

    try to make getStaticPaths generate new paths when revalidate

--- pages/[episode].js
+++ pages/[episode].js
@@ -1,4 +1,4 @@
-import { oneEpisode } from '../util.js'
+import { oneEpisode, allEpisodes } from '../util.js'
 import Player from '../components/Player/Player.js'
 
 export default function EpisodePage({ episode }) {
@@ -31,44 +31,25 @@ export async function getStaticProps(context) {
 
 export async function getStaticPaths() {
 
-    /*
-    const wacky = await fetch('https://champion-weasel-77.hasura.app/v1/graphql', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query: `
-            query MyQuery {
-                podcast_episode_aggregate {
-                    nodes {
-                        episode_num
-                    }
-                }
-            }`
-        }),
-    })
+    const episodes = await allEpisodes()
 
-    const res = await wacky.json()
-    const episode_numbers = res.data.podcast_episode_aggregate.nodes
-
-    const paths = episode_numbers.map((ep) => {
+    const paths = episodes.map(ep => {
         return {
-            params: {
-                episode: ep.episode_num
-            }
+            params: { episode: ep.episode_num }
         }
     })
-    */
 
-    let nums = []
+    //let nums = []
 
-    for (let i=1; i<20; i++) {
-        nums.push(i.toString().padStart(3, '0'))
-    }
+    //for (let i=1; i<20; i++) {
+        //nums.push(i.toString().padStart(3, '0'))
+    //}
 
-    const paths = nums.map(num => {
-        return {
-            params: { episode: num }
-        }
-    })
+    //const paths = nums.map(num => {
+        //return {
+            //params: { episode: num }
+        //}
+    //})
 
     return {
         paths,
