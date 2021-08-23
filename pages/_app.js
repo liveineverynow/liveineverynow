c9ded3d7c803225df2956ef74b5a5099f3d29ed0
Will Hanlen <will.hanlen@gmail.com>
2021-08-22 19:36:11 -0600

    setup storybook and remove old files

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,8 @@
 import '../styles/global.css'
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://champion-weasel-77.hasura.app/v1/graphql',
-  cache: new InMemoryCache()
-});
-
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
       <Component {...pageProps} />
-    </ApolloProvider>
   )
 }
 
