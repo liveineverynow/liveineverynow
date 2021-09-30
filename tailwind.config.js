007a8963ea1316cd1109020181ceadc1906be352
Will Hanlen <will.hanlen@gmail.com>
2021-09-29 20:10:26 -0600

    include jsx files in tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   purge: [
-    // This is not present inside the default configuration
-    // but it's good to build your production application
-    // Read more about this here: https://tailwindcss.com/docs/installation#building-your-css
-    './pages/**/*.js',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: false,
   theme: {
